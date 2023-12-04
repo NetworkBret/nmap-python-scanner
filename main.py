1415bb154e9b97f331369419e76dac02833f1573
NetworkBret <152492914+NetworkBret@users.noreply.github.com>
2023-12-04 05:40:03 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import nmap
+begin = 21
+end = 443
+target = '127.0.0.1'
+scanner = nmap.PortScanner()
+for i in range(begin, end+1):
+	res = scanner.scan(target, str(i))
+	res = res['scan'][target]['tcp'][i]['state']
+	print(f'Port {i} is {res}.')
+
